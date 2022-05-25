a0d312fcb608492b3478e4586cbde2eb82e590ee
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-05-25 16:45:24 +0530

    Dropdown menu content display toggle

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 `use strict`;
+// Nav register active class toggle
 const navRegItems = document.getElementsByClassName("nav-register")[0].children;
 // Removes all 'active' class name from all elements. Then, adds active class to the clicked element.
 const addActiveClass = (element) => {
@@ -9,4 +10,12 @@ const addActiveClass = (element) => {
 // Adds event listener to the nav-register list elements on click.
 Array.from(navRegItems).forEach((element) => {
   element.addEventListener("click", () => addActiveClass(element));
+});
+
+// Dropdown menu content display toggle
+const dropdown = document.getElementsByClassName("nav-menu-dropdown");
+Array.from(dropdown).forEach((element) => {
+  element.addEventListener("click", () => {
+    element.nextElementSibling.classList.toggle("hide");
+  });
 });
